8658c77e680923de2f77d323d8f1dab022c8f23a
Akinsanya Joel <akinsanyajoel82@gmail.com>
2025-08-03 01:42:15 +0100

    Add training logic for recommender and classifier

--- src/book_service/train.py
+++ src/book_service/train.py
@@ -1,9 +1,15 @@
 from sklearn.model_selection import train_test_split
 from book_service.data_manager import load_dataset
 from book_service.pipeline import book_classification_pipeline
-from book_service.config import TRAINING_DATA_FILE,TARGET,FEATURES
-from book_service.config import PIPELINE_SAVE_PATH, TRAINED_MODEL_DIR
 import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from book_service.config import(
+   TRAINING_DATA_FILE,TARGET,
+   FEATURES,PIPELINE_SAVE_PATH,
+   TRAINED_MODEL_DIR,
+   RECOMMENDER_PIPELINE_SAVE_PATH
+   )
 
 
 
@@ -11,6 +17,16 @@ def run_training():
   """Train the model."""
 
   data = load_dataset(file_name=TRAINING_DATA_FILE)
+
+  tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+  tfidf_matrix = tfidf_vectorizer.fit_transform(data['title'])
+  cosine_sim = cosine_similarity(tfidf_matrix)
+
+  recommender_artifacts = {
+    'vectorizer': tfidf_vectorizer,
+    'similarity_matrix': cosine_sim
+}
+  joblib.dump(recommender_artifacts, RECOMMENDER_PIPELINE_SAVE_PATH)
 
   X = data[FEATURES]
   y = data[TARGET]
